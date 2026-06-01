c1c0d012c9667ebf6446cfcc8d4cc8c160d8e371
Ergashev21 <t.ergashev2120@gmail.com>
2026-06-01 18:50:03 +0500

    ------

---
+++ Models/Teacher.cs
@@ -0,0 +1,8 @@
+namespace SchoolManagment.Models;
+public class Teacher
+{
+    public Guid Id {get;set;}
+    public string FullName {get; set;}
+    public string Subjects {get; set;}
+    public int Experience {get; set;}
+}

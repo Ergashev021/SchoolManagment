697dfd7753ebb6bb0e16820c17917e22369339bc
Ergashev021 <ergashevt010@gmail.com>
2026-08-11 09:49:37 +0500

    teacher cs Done

---
+++ Models/Teacher.cs
@@ -0,0 +1,42 @@
+namespace SchoolManagmnent.Models;
+
+public class Teacher
+{
+    public string FullName{get;set;}
+    public string Subjects {get; set;}
+    private int experience;
+    private int qualificationCategory;
+     public int Exprience
+    {
+        get
+        {
+            return experience;
+        }
+        set
+        {
+            if(value < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(value),"Experience can not be negative!!!");
+            }
+            experience = value;
+        }
+    }
+    public int QualificationCategory
+    {
+        get
+        {
+            return qualificationCategory;
+        }
+        set
+        {
+            if (value > 0 && value < 4)
+            {
+                qualificationCategory = value;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(nameof(value),"Between 1 and 3 category!!!");
+            }
+        }
+    }
+}
